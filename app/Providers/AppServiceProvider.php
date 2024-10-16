e87bfd19220b4c82949da4c304aeec436af2923f
binhlt <binhlt@yopaz.vn>
2024-10-16 12:59:39 +0700

    build first project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+
+    }//end register()
+
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+
+    }//end boot()
+
+
+}//end class
